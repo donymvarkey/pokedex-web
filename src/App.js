08c25cd5f9026865e7dc6d590922f0289b5410cf
Dony M Varkey <31372381+donymvarkey@users.noreply.github.com>
2021-03-06 16:26:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
-import {BrowserHistory} from 'react-router/lib/BrowserHistory'
 import './App.css';
 
 import NavBar from './components/layout/NavBar'
